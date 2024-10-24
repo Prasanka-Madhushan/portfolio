429d3576b1809003a655c36fc63e580f26debe20
Prasanka <prasankamadushan0@gmail.com>
2024-10-24 19:12:48 +0530

    Updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -56,58 +56,104 @@ const Contact = () => {
   };
 
   return (
-    <section className="contact" id="contact">
-      <h2 className="heading">Contact <span>Me!</span></h2>
-      <form onSubmit={handleSubmit}>
-        <div className="input-box">
-          <input
-            type="text"
-            name="name"
-            placeholder="Full Name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-          {errors.name && <p className="error">{errors.name}</p>}
-          <input
-            type="email"
-            name="email"
-            placeholder="Email Address"
-            value={formData.email}
-            onChange={handleChange}
-          />
-          {errors.email && <p className="error">{errors.email}</p>}
-        </div>
-        <div className="input-box">
-          <input
-            type="tel"
-            name="telephone"
-            required
-            pattern="[0-9]{10}"
-            placeholder="Mobile Number"
-            value={formData.telephone}
-            onChange={handleChange}
-          />
-          {errors.telephone && <p className="error">{errors.telephone}</p>}
-          <input
-            type="text"
-            name="subject"
-            placeholder="Email Subject"
-            value={formData.subject}
-            onChange={handleChange}
-          />
-        </div>
-        <textarea
-          name="message"
-          cols="30"
-          rows="10"
-          placeholder="Your Message"
-          value={formData.message}
-          onChange={handleChange}
-        ></textarea>
-        {errors.message && <p className="error">{errors.message}</p>}
-        <input type="submit" value="Send Message" className="btn" />
-      </form>
-    </section>
+    <div class="container">
+      <div class="bubbles">
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+        <span class="dot" style={{ '--i': `${Math.random() * 30 + 10}` }}></span>
+
+        <section className="contact" id="contact">
+          <h2 className="heading">Contact <span>Me!</span></h2>
+          <form onSubmit={handleSubmit}>
+            <div className="input-box">
+              <input
+                type="text"
+                name="name"
+                placeholder="Full Name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+              {errors.name && <p className="error">{errors.name}</p>}
+              <input
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              {errors.email && <p className="error">{errors.email}</p>}
+            </div>
+            <div className="input-box">
+              <input
+                type="tel"
+                name="telephone"
+                required
+                pattern="[0-9]{10}"
+                placeholder="Mobile Number"
+                value={formData.telephone}
+                onChange={handleChange}
+              />
+              {errors.telephone && <p className="error">{errors.telephone}</p>}
+              <input
+                type="text"
+                name="subject"
+                placeholder="Email Subject"
+                value={formData.subject}
+                onChange={handleChange}
+              />
+            </div>
+            <textarea
+              name="message"
+              cols="30"
+              rows="10"
+              placeholder="Your Message"
+              value={formData.message}
+              onChange={handleChange}
+            ></textarea>
+            {errors.message && <p className="error">{errors.message}</p>}
+            <input type="submit" value="Send Message" className="btn" />
+          </form>
+        </section>
+      </div>
+    </div>
   );
 };
 
